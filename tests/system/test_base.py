bd12c630c3b7f538c2ef5c3f00f0a9e7b818f73d
Don Pattee <x-caiver@x-caiver.com>
2020-09-07 19:30:08 -0700

    Initial commit, Add generated files

---
+++ tests/system/test_base.py
@@ -0,0 +1,19 @@
+from speedbeat import BaseTest
+
+import os
+
+
+class Test(BaseTest):
+
+    def test_base(self):
+        """
+        Basic test with exiting {Beat} normally
+        """
+        self.render_config_template(
+            path=os.path.abspath(self.working_dir) + "/log/*"
+        )
+
+        speedbeat_proc = self.start_beat()
+        self.wait_until(lambda: self.log_contains("speedbeat is running"))
+        exit_code = speedbeat_proc.kill_and_wait()
+        assert exit_code == 0
